2e3b3a248c5b675c6fb1c2ea14ce9d24f39d9956
Abass Okikiokluwa <abassokikioluwa731@gmail.com>
2023-05-31 15:06:10 +0100

    This is the first update on this project

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+	$('.dropdown1').addClass('show');
+	const attr = document.querySelector('.dropdown1').getAttribute('aria-expanded').value = 'true';	
+	
+});
